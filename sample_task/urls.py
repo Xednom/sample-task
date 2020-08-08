d10f67015f254166bee30aaf659c067d9abddf9e
Xednom <monde.lacanlalay@gmail.com>
2020-08-08 17:29:37 +0800

    added the homeview in urls of the project

--- sample_task/urls.py
+++ sample_task/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from app.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path('api/v1/', include("api.urls")),
+    path('', HomeView.as_view(), name="Home")
 ]
